eb61f2b6909e7864eccd4bf96d68e6fdd624eee2
skutas23 <skutas23@Z00P1-04>
2025-10-08 10:10:13 +0200

    kalkulacka

--- piotrek/urls.py
+++ piotrek/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tuniak/', include('tuniak.urls')),
+    path('kalkulacka/', include('kalkulacka.urls')),
     ]
